40b416d649929d7f812e1b8d7cb6b99f2128fbdb
vm <vm@desk.top>
2025-01-25 00:52:16 +0900

    vm 수정

--- channel_move.py
+++ channel_move.py
@@ -38,8 +38,8 @@ def on_release(key):
         
 def target_process(run_flag):
     pag.FAILSAFE = False
-    is_notebook = True
     is_notebook = False
+    is_vm = True
     # n번째 유저
     n_user = 1
     
@@ -49,20 +49,29 @@ def target_process(run_flag):
         x2, y2 = x + 110, y + 75
         x3, y3 = x - 650, y + 315 - n_differ*(n_user-1)
         x4, y4 = x - 550, y + 330 - n_differ*(n_user-1)
-        notebook_delay = 0.5
+        add_delay = 0.5
+
+    elif not is_notebook and is_vm:
+        n_differ = 50
+        x, y = 1400, 200 + n_differ*(n_user-1)
+        x2, y2 = x + 65, y + 70
+        x3, y3 = x - 650, y + 285 - n_differ*(n_user-1)
+        x4, y4 = x - 550, y + 300 - n_differ*(n_user-1)
+        add_delay = 0.5
+
     else:
         n_differ = 80
         x, y = 2150, 330 + n_differ*(n_user-1)
         x2, y2 = x + 120, y + 100
         x3, y3 = x - 1050, y + 470 - n_differ*(n_user-1)
         x4, y4 = x - 900, y + 500 - n_differ*(n_user-1)
-        notebook_delay = 0.5
+        add_delay = 0.5
         
     while run_flag.value:
         mouse_click(x, y)                           # 유저
         mouse_click(x2, y2)                         # 같이 플레이 하기
-        mouse_click(x3, y3, 2.5 + notebook_delay)   # 네
-        mouse_click(x4, y4, 3.0 + notebook_delay)   # 돌아가기
+        mouse_click(x3, y3, 2.5 + add_delay)   # 네
+        mouse_click(x4, y4, 3.0 + add_delay + 3)   # 돌아가기
 
 if __name__ == "__main__":
     run_flag = Value('i', False)
